b84f95bb1a99297997ea477f3bc9fe988e4ee75d
Oscar <ortizj2289@yahoo.com>
2024-08-23 08:30:39 -0500

    completed to-do list tasks and updated the list

--- blackjack.cpp
+++ blackjack.cpp
@@ -1,9 +1,8 @@
-//TO-DO LIST
-//1. Fix the validation while-loop on line 132. If a character is entered, it creates an infinite loop.
-//2. Implement a way to decide when an Ace is either an 11 or a 1.
+﻿//TO-DO LIST
+//1. Implement a way to decide when an Ace is either an 11 or a 1.
 //	 Idea: Store the values of the cards drawn in an array and search for an 11
 //	 when appropriate, delete it, and replace it with a 1.
-//3. Fix when the dealer should continue drawing on line 177.
+//2. Create a vector of arrays to hold ascii art for each card. Display card images instead of card names.
 
 //HEADER FILES
 #include <iostream>
@@ -11,13 +10,15 @@
 #include <time.h>
 #include <string>
 #include <vector>
+//#include "cardImages.cpp"
 using namespace std;
 
 //FUNCTION PROTOTYPES
-void exitProgram();
 void letsPlay();
-string drawCard(vector<string> &, vector<int> &, int &, int &);
+void exitProgram();
+void compareHands(int, int);
 void showDeck(vector<string>, int);	//FOR TESTING
+string drawCard(vector<string>&, vector<int>&, int&, int&);
 
 //MAIN FUNCTION THAT DISPLAYS THE WELCOME MESSAGE
 int main() {
@@ -25,23 +26,18 @@ int main() {
 	srand(time(0));		//SEED THE RANDOM NUMBER GENERATOR
 	string playAgain;	//ACCEPTS THE USER RESPONSE TO PLAY
 
-	cout << "****************************" << endl;
-	cout << "**                        **" << endl;
-	cout << "**  WELCOME TO BLACKJACK  **" << endl;
-	cout << "**                        **" << endl;
-	cout << "****************************" << endl;
+	cout << "								   " << endl;
+	cout << "	   ****************************" << endl;
+	cout << "	   **                        **" << endl;
+	cout << "	   **  WELCOME TO BLACKJACK  **" << endl;
+	cout << "	   **                        **" << endl;
+	cout << "	   ****************************" << endl;
 
-	cout << "\nWould you like to play? (yes/no): ";
+	cout << "\nEnter any key to play, type EXIT to close the program...";
 	cin >> playAgain;
-	
-	//VALIDATE RESPONSE INPUT
-	while (playAgain != "Yes" && playAgain != "yes" && playAgain != "Y" && playAgain != "y" && playAgain != "No" && playAgain != "no" && playAgain != "N" && playAgain != "n") {
-		cout << "Only Enter 'Yes' or 'No': ";
-		cin >> playAgain;
-	}
 
 	//USE RESPONSE INPUT TO EXIT THE GAME OR START THE GAME
-	if (playAgain == "No" || playAgain == "no" || playAgain == "N" || playAgain == "n") {
+	if (playAgain == "EXIT" || playAgain == "exit") {
 		exitProgram();
 	}
 	else {
@@ -58,9 +54,9 @@ int main() {
 void letsPlay() {
 	//PARALLEL VECTORS TO STORE THE NAMES AND VALUES OF THE CARDS
 	vector<string> deckOfCards_face{ "Two of Spades", "Three of Spades", "Four of Spades", "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades", "Nine of Spades", "Ten of Spades", "Jack of Spades", "Queen of Spades", "King of Spades", "Ace of Spades",
-								 "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Hearts",
-								 "Two of Clubs", "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs", "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Clubs",
-								 "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds", "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds", "Ace of Diamonds" };
+									 "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Hearts",
+									 "Two of Clubs", "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs", "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Clubs",
+									 "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds", "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds", "Ace of Diamonds" };
 	vector<int> deckOfCards_value{ 2,3,4,5,6,7,8,9,10,10,10,10,11,
 								   2,3,4,5,6,7,8,9,10,10,10,10,11,
 								   2,3,4,5,6,7,8,9,10,10,10,10,11,
@@ -69,7 +65,7 @@ void letsPlay() {
 	int size = deckOfCards_face.size();	//SIZE OF THE VECTORS. THE SIZE ADJUSTS WHEN THE DRAWCARD() FUNCTION IS CALLED.
 
 	//RUNNING TOTAL VARIABLES TO KEEP TRACK OF THE DEALER AND PLAYER HANDS.
-	//WHEN THE DRAWCARD() FUNCTION IS CALLED, THE APPROPRIATE VARIABLE(HAND) MUST BE HANDED TO THE FUNCTION TO UPDATE THE VARIABLE.
+	//WHEN THE DRAWCARD() FUNCTION IS CALLED, THE APPROPRIATE VARIABLE(HAND) IS HANDED TO THE FUNCTION TO UPDATE THE VARIABLE.
 	int dealerHand = 0;
 	int playerHand = 0;
 	
@@ -87,27 +83,14 @@ void letsPlay() {
 	cout << endl;
 	system("pause");
 
-	//DISPLAY EACH PLAYER'S HAND
+	//DISPLAY EACH PLAYER'S HAND TOTAL
 	cout << "\nDealer has: " << dealerHand << " + [Facedown card]" << endl;
 	cout << "You have: " << playerHand << endl;
 	cout << endl;
 	system("pause");
 
-	if (playerHand == 21 && dealerHand < 21) {
-		cout << "You got 21, YOU WIN! Thanks for playing." << endl;
-		exitProgram();
-	}
-
-	if (dealerHand == 21 && playerHand < 21) {
-		cout << "Dealer got 21, YOU LOSE! Thanks for playing." << endl;
-		exitProgram();
-	}
-
-	if (playerHand == 21 && dealerHand == 21) {
-		cout << "Its a draw! Thanks for playing." << endl;
-		exitProgram();
-	}
-	
+	//CHECK FOR A WINNER
+	compareHands(playerHand, dealerHand);
 	
 	//SINCE, TECHNICALLY, ONLY THREE CARDS HAVE BEEN DRAWN AT THIS POINT,
 	//WE CAN CALL THE SHOWDECK() FUNCTION WITH OPTION 4 BELOW, AND IT WILL
@@ -125,96 +108,60 @@ void letsPlay() {
 		cout << "****************************************" << endl;
 
 		cout << "\nEnter choice: ";
-		int actionChoice;
+		string actionChoice;
 		cin >> actionChoice;
 		
 		//VALIDATE THE CHOICE
-		while (actionChoice != 1 && actionChoice != 2 && actionChoice != 3 && actionChoice != 4) {	//NEEDS TO BE FIXED
+		while (actionChoice != "1" && actionChoice != "2" && actionChoice != "3" && actionChoice != "4") {	//NEEDS TO BE FIXED
 			cout << "Only Enter 1 or 2: " << endl;
 			cin >> actionChoice;
 		}
 
+		//CONVERT THE STRING TO AN INTEGER SO THAT IT IS COMPATIBLE WITH THE SWITCH STATEMENT BELOW.
+		int choice = stoi(actionChoice);
+
 		//EXECUTE THE CHOICE
-		switch (actionChoice) {
-		case 1:
-			cout << "\nYou drew: ";
-			cout << drawCard(deckOfCards_face, deckOfCards_value, size, playerHand) << endl;
-			cout << "\nDealer has " << dealerHand << " + [Facedown card]" << endl;
-			cout << "You have: " << playerHand << endl;
-			system("pause");
+		switch (choice) {
+		case 1:	//DISPLAY WHAT THE PLAYER DREW AND DISPLAY HAND TOTALS
+				cout << "\nYou drew: ";
+				cout << drawCard(deckOfCards_face, deckOfCards_value, size, playerHand) << endl;
+				cout << "\nDealer has " << dealerHand << " + [Facedown card]" << endl;
+				cout << "You have: " << playerHand << endl;
+				system("pause");
 
-			if (playerHand >= 22) {
-				cout << "\nYou bust. YOU LOSE!" << endl;
-				cout << endl;
-				exitProgram();
-			}
-
-			break;
-		case 2:
-			//DEALER REVEALS FACEDOWN CARD
-			cout << "\nDealer flips card..." << endl;
-			cout << drawCard(deckOfCards_face, deckOfCards_value, size, dealerHand) << endl;
-			cout << "\nDealer has: " << dealerHand << endl;
-			cout << endl;
-
-			system("pause");
-
-			//EXIT PROGRAM IF DEALER WINS WITH 21
-			if (dealerHand == 21) {
-				cout << "\nDealer has 21. YOU LOSE!" << endl;
-				cout << endl;
-				exitProgram();
-			}
-
-			//EXIT PROGRAM IF PLAYER HAND IS GREATER THAN DEALER HAND
-			if (dealerHand < playerHand && dealerHand >= 17) {
-				cout << "YOU WIN!" << endl;
-				cout << endl;
-				exitProgram();
-			}
-
-			//DEALER DRAWS UNTIL NECESSARY
-			while (dealerHand < 17) {
-				cout << "\nDealer draws..." << endl;
-				cout << drawCard(deckOfCards_face, deckOfCards_value, size, dealerHand);
-				cout << "\nDealer has: " << dealerHand << endl;
-				cout << "Player has: " << playerHand << endl;
-
-				//EXIT PROGRAM IF DEALER WINS WITH 21
-				if (dealerHand == 21) {
-					cout << "\nDealer has 21. YOU LOSE!" << endl;
-					cout << endl;
-					exitProgram();
-				}
-
-				//EXIT PROGRAM IF DEALER HAND IS GREATER THAN PLAYER HAND
-				if (dealerHand > playerHand && dealerHand < 21) {
-					cout << "\nDealer hand wins. YOU LOSE!" << endl;
-					cout << endl;
-					exitProgram();
-				}
-
-				//EXIT PROGRAM IF DEALER BUSTS
-				if (dealerHand > 21) {
-					cout << "\nDealer Busts! YOU WIN!" << endl;
-					cout << endl;
-					exitProgram();
-				}
-
-				//EXIT PROGRAM IF PLAYER HAND IS GREATER THAN DEALER HAND
-				if (playerHand > dealerHand) {
-					cout << "YOU WIN!" << endl;
-					cout << endl;
-					exitProgram();
-				}
-			}
-			break;
-		
-		//FOR TESTING PURPOSES
-		case 3: showDeck(deckOfCards_face, size);
+				//CHECK FOR A WINNER
+				compareHands(playerHand, dealerHand);
 				break;
-		//FOR TESTING PURPOSES
-		case 4: drawCard(deckOfCards_face, deckOfCards_value, size, playerHand);
+
+		case 2: //DEALER REVEALS FACEDOWN CARD
+				cout << "\nDealer flips card..." << endl;
+				cout << drawCard(deckOfCards_face, deckOfCards_value, size, dealerHand) << endl;
+				cout << "\nDealer has: " << dealerHand << endl;
+				cout << endl;
+
+				system("pause");
+
+				//CHECK FOR A WINNER
+				compareHands(playerHand, dealerHand);
+
+				//DEALER DRAWS UNTIL NECESSARY
+				while (dealerHand < 17) {
+					cout << "\nDealer draws..." << endl;
+					cout << drawCard(deckOfCards_face, deckOfCards_value, size, dealerHand);
+					cout << "\nDealer has: " << dealerHand << endl;
+					cout << "Player has: " << playerHand << endl;
+
+					system("pause");
+					compareHands(playerHand, dealerHand);
+				}
+				break;
+
+		case 3: //FOR TESTING PURPOSES
+				showDeck(deckOfCards_face, size);
+				break;
+
+		case 4: //FOR TESTING PURPOSES
+				drawCard(deckOfCards_face, deckOfCards_value, size, playerHand);
 				break;
 		}
 	} while (true);
@@ -247,4 +194,51 @@ void showDeck(vector<string> deck, int size) {
 	}
 	system("pause");
 	cout << endl;
+}
+
+//FUNCTION DEFINITION FOR CHECKING ALL HANDS FOR A WINNER
+void compareHands(int playerHand, int dealerHand) {
+	//CHECK FOR BLACKJACK - PLAYER
+	if (playerHand == 21 && dealerHand != 21) {
+		cout << "\nYou got 21, YOU WIN! Thanks for playing." << endl;
+		exitProgram();
+	}
+	//CHECK FOR BLACKJACK - DEALER
+	if (dealerHand == 21 && playerHand != 21) {
+		cout << "\nDealer has 21, YOU LOSE! Thanks for playing." << endl;
+		exitProgram();
+	}
+
+	//CHECK FOR BUST - PLAYER
+	if (playerHand >= 22 && dealerHand <= 21) {
+		cout << "\nYou bust. YOU LOSE!" << endl;
+		cout << endl;
+		exitProgram();
+	}
+	//CHECK FOR BUST - DEALER
+	if (dealerHand >= 22 && playerHand <= 21) {
+		cout << "\nDealer busts! YOU WIN!" << endl;
+		cout << endl;
+		exitProgram();
+	}
+
+	//CHECK FOR GREATER HAND - PLAYER
+	if (dealerHand >= 17 && dealerHand < playerHand && playerHand < 21) {
+		cout << "YOU WIN!" << endl;
+		cout << endl;
+		exitProgram();
+	}
+
+	//CHECK FOR GREATER HAND - DEALER
+	if (dealerHand >= 17 && dealerHand > playerHand && dealerHand < 21) {
+		cout << "\nDealer hand wins. YOU LOSE!" << endl;
+		cout << endl;
+		exitProgram();
+	}
+
+	//CHECK FOR DRAW
+	if (playerHand == dealerHand && dealerHand >= 17) {
+		cout << "Its a draw! Thanks for playing." << endl;
+		exitProgram();
+	}
 }
